a3e6e6387c7f9f0f7ececca4b4ed90462fae4781
Ojas Kulkarni <72272267+0-Playor-0@users.noreply.github.com>
2024-04-15 23:28:15 +0530

    Last Commit

--- FitnessApp/app.py
+++ FitnessApp/app.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_socketio import SocketIO, emit
 
+import io
 
 from werkzeug.utils import secure_filename
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
+import cv2
+import numpy as np
 
+import pytesseract
+#pytesseract.pytesseract.tesseract_cmd = r'C:\USers\Ashish\AppData\Local\Program Files\Tesseract-OCR\tesseract.exe'
 
 import requests
 
 import datetime
+from datetime import timezone
 import os
 
 
@@ -18,6 +24,202 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+food_calories = {
+    "artichoke": 60,
+    "arugula": 1,
+    "asparagus": 2,
+    "aubergine": 115,
+    "beetroot": 35,
+    "bell pepper": 15,
+    "black olives": 2,
+    "broccoli": 207,
+    "brussels sprouts": 8,
+    "cabbage": 227,
+    "capsicum": 12,
+    "carrot": 25,
+    "cauliflower": 3,
+    "celery": 6,
+    "chard": 9,
+    "cherry tomato": 20,
+    "chicory": 38,
+    "chinese cabbage": 134,
+    "chives": 1,
+    "collard greens": 12,
+    "corn": 562,
+    "courgette": 33,
+    "creamed spinach": 148,
+    "cucumber": 66,
+    "eggplant": 115,
+    "endive": 87,
+    "fennel": 73,
+    "garlic": 4,
+    "gherkin": 9,
+    "gourd": 108,
+    "green beans": 34,
+    "green olives": 2,
+    "green onion": 5,
+    "horseradish": 7,
+    "kale": 33,
+    "kohlrabi": 108,
+    "kumara": 112,
+    "leek": 54,
+    "lettuce": 90,
+    "mushrooms": 1,
+    "mustard greens": 15,
+    "nori": 1,
+    "okra": 4,
+    "olives": 2,
+    "onion": 34,
+    "parsnips": 128,
+    "peas": 79,
+    "pepper": 20,
+    "potato": 164,
+    "pumpkin": 51,
+    "radishes": 1,
+    "red cabbage": 7,
+    "rutabaga": 147,
+    "shallots": 18,
+    "spinach": 78,
+    "squash": 88,
+    "sweet potato": 112,
+    "tomato": 20,
+    "turnip greens": 34,
+    "turnips": 34,
+    "wasabi": 184,
+    "winter squash": 147,
+    "zucchini": 33,
+    "acai": 20,
+    "apple": 95,
+    "applesauce": 167,
+    "apricot": 17,
+    "avocado": 320,
+    "banana": 111,
+    "blackberries": 62,
+    "blood oranges": 70,
+    "blueberries": 84,
+    "cantaloupe": 23,
+    "cherries": 4,
+    "clementine": 35,
+    "cranberries": 46,
+    "currants": 63,
+    "custard apple": 136,
+    "dates": 20,
+    "figs": 37,
+    "fruit salad": 125,
+    "grapes": 104,
+    "greengage": 2,
+    "guava": 37,
+    "jackfruit": 143,
+    "jujube": 22,
+    "kiwi": 112,
+    "lemon": 17,
+    "lime": 20,
+    "lychees": 7,
+    "mandarin oranges": 47,
+    "mango": 202,
+    "minneola": 70,
+    "mulberries": 60,
+    "nectarine": 66,
+    "orange": 62,
+    "papaya": 215,
+    "passion fruit": 17,
+    "peach": 59,
+    "pear": 101,
+    "persimmon": 32,
+    "physalis": 2,
+    "pineapple": 453,
+    "plantains": 218,
+    "plum": 30,
+    "pomegranate": 234,
+    "quince": 52,
+    "raisins": 434,
+    "rambutan": 7,
+    "raspberries": 64,
+    "rhubarb": 11,
+    "starfruit": 28,
+    "strawberries": 49,
+    "tamarind": 5,
+    "tangerine": 47,
+    "watermelon": 86,
+    "baby back ribs": 360,
+    "bacon and eggs": 539,
+    "baked beans": 244,
+    "bbq ribs": 360,
+    "beef stew": 186,
+    "biryani": 484,
+    "black pudding": 101,
+    "black rice": 323,
+    "blt": 593,
+    "brown rice": 670,
+    "burrito": 326,
+    "butter chicken": 490,
+    "california roll": 33,
+    "chicken caesar salad": 392,
+    "chicken fried steak": 423,
+    "chicken marsala": 2209,
+    "chicken parmesan": 250,
+    "chicken pot pie": 673,
+    "chicken tikka masala": 195,
+    "chili con carne": 266,
+    "chimichanga": 418,
+    "cobb salad": 632,
+    "collard greens": 13,
+    "corn dog": 438,
+    "corned beef hash": 349,
+    "cottage pie": 523,
+    "dal": 304,
+    "deviled eggs": 62,
+    "dim sum": 37,
+    "dosa": 287,
+    "enchiladas": 323,
+    "fajita": 290,
+    "fish and chips": 585,
+    "fried rice": 662,
+    "fried shrimp": 75,
+    "grilled cheese sandwich": 392,
+    "ham and cheese sandwich": 352,
+    "hummus": 435,
+    "jambalaya": 250,
+    "kebab": 774,
+    "lasagne": 284,
+    "mac and cheese": 699,
+    "macaroni and cheese": 699,
+    "mashed potatoes": 174,
+    "meat pie": 290,
+    "meatloaf": 721,
+    "naan": 260,
+    "orange chicken": 420,
+    "pad thai": 375,
+    "paella": 200,
+    "paratha": 260,
+    "pea soup": 190,
+    "peanut butter sandwich": 200,
+    "peking duck": 401,
+    "philly cheese steak": 300,
+    "pizza": 272,
+    "pork chop": 295,
+    "potato salad": 136,
+    "pulled pork sandwich": 551,
+    "ramen": 380,
+    "ravioli": 134,
+    "reuben sandwich": 641,
+    "roast beef": 23,
+    "roast dinner": 240,
+    "samosa": 107,
+    "sausage roll": 361,
+    "sausage rolls": 101,
+    "shepherds pie": 159,
+    "shrimp cocktail": 130,
+    "sloppy joe": 101,
+    "sloppy joes": 397,
+    "spaghetti bolognese": 374,
+    "spring roll": 350,
+    "spring rolls": 350,
+    "taco": 213,
+    "tandoori chicken": 198,
+    "yorkshire pudding": 164
+}
 
 
 existingChats = []
@@ -29,7 +231,10 @@ def index():
         quote = get_random_quote()
         greeting = get_greeting()
         posts = get_posts()
-        return render_template('index.html', quote=quote, greeting=greeting, username=username, posts=posts)
+        users = get_users()
+        total_calories = get_total_calories()
+        step_count = get_total_steps() 
+        return render_template('index.html', quote=quote, greeting=greeting, username=username, posts=posts, users=users, total_calories=total_calories, step_count=step_count)  
     return redirect(url_for('login'))
 
 
@@ -72,13 +277,17 @@ def profile():
             height = request.form.get('height', '')
             goal = request.form.get('goal', '')
             activity = request.form.get('activity', '')
-            users[username]['age'] = age
-            users[username]['location'] = location
-            users[username]['weight'] = weight
-            users[username]['height'] = height
-            users[username]['goal'] = goal
-            users[username]['activity'] = activity
-            users[username]['points']
+            points = users[username]['points']  
+            users[username] = {
+                'password': users[username]['password'],
+                'age': age,
+                'location': location,
+                'weight': weight,
+                'height': height,
+                'goal': goal,
+                'activity': activity,
+                'points': points
+            }
             with open('users.txt', 'w') as file:
                 for user, data in users.items():
                     file.write(f"{user}:{data['password']}:{data['age']}:{data['location']}:{data['weight']}:{data['height']}:{data['goal']}:{data['activity']}:{data['points']}\n")
@@ -113,6 +322,113 @@ def add_post():
                 caption = request.form.get('caption', '')
                 save_post(filename, caption, username)
     return redirect(url_for('index'))
+
+def add_comment(post_index, comment):
+    posts = get_posts()
+    if post_index < len(posts):
+        posts[post_index]['comments'].append(comment)
+        update_posts(posts)
+
+@app.route('/like_post/<int:post_index>', methods=['POST'])
+def like_post(post_index):
+    if 'username' in session:
+        posts = get_posts()
+        username = session['username']
+        if post_index < len(posts):
+            post = posts[post_index]
+            if 'liked_posts' not in session:
+                session['liked_posts'] = []
+            if post_index not in session['liked_posts']:
+                post['likes'] += 1
+                session['liked_posts'].append(post_index)
+                update_posts(posts)
+        return redirect(url_for('index'))
+    return redirect(url_for('login'))
+
+@app.route('/comment_post/<int:post_index>', methods=['POST'])
+def comment_post(post_index):
+    if 'username' in session:
+        comment = request.form.get('comment', '')
+        if comment:
+            add_comment(post_index, comment)
+    return redirect(url_for('index'))
+
+def update_posts(posts):
+    with open('posts.txt', 'w') as file:
+        for post in posts:
+            comments = ','.join(post.get('comments', []))  
+            file.write(f"{post['image'].split('/')[-1]}:{post['caption']}:{post['username']}:{post['likes']}:{comments}\n")
+
+@app.route('/videos')
+def search_videos():
+    activity = request.args.get('activity')
+    if not activity:
+        return jsonify({'error': 'No activity provided'}), 400
+
+    api_key = 'AIzaSyCa8CbpmSUMPu7yBkY0LXpCCpJl_KwufLQ'
+  
+    query = f'{activity} stock video'
+    url = f'https://www.googleapis.com/youtube/v3/search?q={query}&key={api_key}&part=snippet&type=video&maxResults=1'
+    response = requests.get(url)
+    data = response.json()
+
+    return jsonify(data)
+
+
+@app.route('/add_food', methods=['POST'])
+def add_food():
+    food_item = request.form['food_item']
+    if food_item.lower() in food_calories:
+        calories = food_calories[food_item.lower()]
+        tracker_filename = f'tracker_{session["username"]}.txt'
+        with open(tracker_filename, 'a') as tracker_file:
+            tracker_file.write(f"Food:{food_item.capitalize()}:{calories}\n")
+        return redirect(url_for('index'))
+    else:
+        return "Food item not found in the database"
+
+@app.route('/share_post/<int:post_index>')
+def share_post(post_index):
+    
+    return redirect(url_for('index'))
+
+
+@app.route('/sleep', methods=['GET', 'POST'])
+def sleep():
+    if 'username' in session:
+        username = session['username']
+        if request.method == 'POST':
+            sleep_start_str = request.form.get('sleep_start', '')
+            sleep_end_str = request.form.get('sleep_end', '')
+            if sleep_start_str and sleep_end_str:
+                try:
+                    sleep_start = datetime.datetime.strptime(sleep_start_str, '%Y-%m-%dT%H:%M')
+                    sleep_end = datetime.datetime.strptime(sleep_end_str, '%Y-%m-%dT%H:%M')
+                    if sleep_end > sleep_start:
+                        sleep_duration = sleep_end - sleep_start
+                        hours, remainder = divmod(sleep_duration.seconds, 3600)
+                        minutes, _ = divmod(remainder, 60)
+                        total_hours = hours + (minutes / 60)
+                        print(total_hours)
+                        points_earned = int(total_hours) * 10
+                        print(points_earned)
+                        tracker_filename = f'tracker_{username}.txt'
+                        if not os.path.exists(tracker_filename):
+                            with open(tracker_filename, 'w') as tracker_file:
+                                tracker_file.write('Sleep tracker\n\n')
+                        with open(tracker_filename, 'a') as tracker_file:
+                            tracker_file.write(f"{sleep_start.strftime('%Y-%m-%d %H:%M:%S')} - {sleep_end.strftime('%Y-%m-%d %H:%M:%S')} ({total_hours:.1f} hours)\n")
+                        users = get_users()
+                        users[username]['points'] = str(int(users[username]['points']) + points_earned)
+                        update_users(users)
+                        return redirect(url_for('index'))
+                    else:
+                        return 'End time should be after start time'
+                except ValueError:
+                    return 'Invalid date format'
+        return render_template('sleep.html')
+    return redirect(url_for('login'))
+
 
 rooms = {}
 UserData = None
@@ -275,17 +591,19 @@ def leaderboard():
     print(position+1)
     return render_template('leaderboard.html', leaderboard=leaderboard_data)
 
-
+@app.route('/step_tracker', methods=['POST'])
+def step_tracker():
+    step_count = get_step_count()
+    return step_count
 
 #Extra Code for API's and easy access ---------------------------------------------------------------------------->
-
 
 def get_users():
     with open('users.txt', 'r') as file:
         users = {}
         for line in file.readlines():
             user_data = line.strip().split(':')
-            if len(user_data) >= 7:
+            if len(user_data) >= 9:
                 username = user_data[0]
                 password = user_data[1]
                 age = user_data[2]
@@ -303,15 +621,23 @@ def get_users():
                     'height': height,
                     'goal': goal,
                     'activity': activity,
-                    'points':points
+                    'points': points
                 }
                 users[username] = user_info
         return users
-
-def save_user(username, password, age='', location='', weight='', height='', goal='', activity=''):
+    
+def save_user(username, password, age='', location='', weight='', height='', goal='', activity='', points='0'):
+    tracker_filename = f'tracker_{username}.txt'
+    if not os.path.exists(tracker_filename):
+        with open(tracker_filename, 'w') as tracker_file:
+            tracker_file.write('Sleep tracker\n\n')
     with open('users.txt', 'a') as file:
-        file.write(f'{username}:{password}:{age}:{location}:{weight}:{height}:{goal}:{activity}\n')
+        file.write(f'{username}:{password}:{age}:{location}:{weight}:{height}:{goal}:{activity}:{points}\n')
 
+def update_users(users):
+    with open('users.txt', 'w') as file:
+        for username, data in users.items():
+            file.write(f"{username}:{data['password']}:{data['age']}:{data['location']}:{data['weight']}:{data['height']}:{data['goal']}:{data['activity']}:{data['points']}\n")
 
 def get_random_quote():
     response = requests.get('https://api.quotable.io/random')
@@ -333,24 +659,58 @@ def get_greeting():
 
 def get_posts():
     posts = []
-    
     if os.path.exists('posts.txt'):
         with open('posts.txt', 'r') as file:
             for line in reversed(file.readlines()): 
                 post_data = line.strip().split(':')
-                print(url_for('static', filename='uploads/' + post_data[0]))
-                if len(post_data) >= 3:
+                if len(post_data) >= 4:  
                     post = {
-                        'image':url_for('static', filename='uploads/' + post_data[0]),
+                        'image': url_for('static', filename='uploads/' + post_data[0]),
                         'caption': post_data[1],
-                        'username': post_data[2]
+                        'username': post_data[2],
+                        'likes': int(post_data[3]),
+                        'index': len(posts),
+                        'comments': post_data[4].split(',')
                     }
                     posts.append(post)
     return posts
 
 def save_post(image_filename, caption, username):
     with open('posts.txt', 'a') as file:
-        file.write(f'{image_filename}:{caption}:{username}\n')
+        file.write(f'{image_filename}:{caption}:{username}:0:\n')
+
+
+
+from PIL import Image    
+
+def get_total_calories():
+    tracker_filename = f'tracker_{session["username"]}.txt'
+    total_calories = 0
+    with open(tracker_filename, 'r') as tracker_file:
+        for line in tracker_file:
+            parts = line.strip().split(':')
+            if len(parts) == 3 and parts[0] == 'Food':
+                try:
+                    calories = int(parts[2])
+                    total_calories += calories
+                except ValueError:
+                    pass
+    return total_calories   
+
+
+def get_total_steps():
+    tracker_filename = f'tracker_{session["username"]}.txt'
+    total_steps = 0
+    with open(tracker_filename, 'r') as tracker_file:
+        for line in tracker_file:
+            parts = line.strip().split(':')
+            if len(parts) == 2 and parts[0] == 'Steps':
+                try:
+                    steps = int(parts[1])
+                    total_steps += steps
+                except ValueError:
+                    pass
+    return total_steps
 
 import json
 import requests
@@ -361,6 +721,7 @@ import re
 
 @socketio.on("message")
 def message(data):
+    session['feeling'] = "okay"
     # Update user points
     if 'message' not in data:
         print("Error: 'message' property is missing in the data")
@@ -433,22 +794,35 @@ def message(data):
             if value.strip() == 'True':
                 Flag = key.strip()
                 break
+    feeling_content = {}
     if Flag == None:        
         session['points'] += 5
         if not data.get("message").strip().lower().startswith("@fitbot"):
             session['feeling'] = "Ok"
     else:
         session['feeling'] = Flag
+        if Flag == "liwc_negative_emotion":
+            Flag = "negative"
+        # Prepare content for Fitbot response
+        feeling_content = {
+            "name": "Fitbot",
+            "message": "Hey! are you feeling alright? "+ session['username'] +". We have noticed that you are feeling " + Flag + ". Would you like to talk about it?"
+        }
+        
+        
     
     send(content, to=room)
     rooms[room]["messages"].append(content)
     print(f"{session.get('username')} said: {data.get('message', '')}")
 
+    # Send Fitbot response to the room
+    send(feeling_content, to=room)
+    rooms[room]["messages"].append(feeling_content)
+
     # Check if the message starts with "@Fitbot" (case insensitive)
     if data.get("message").strip().lower().startswith("@fitbot"):
         import openai
 
-        openai.api_key = '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'
         # Call ChatGPT API to generate a response
         input_text = f"Scenario: I am feeling {session['feeling']}.Keep what im feeling in mind while responding Query: " + data.get("message")
         input_text = str(input_text)        
@@ -471,7 +845,6 @@ def message(data):
     if data.get("message").strip().lower().startswith("@picbot"):
         import openai
 
-        openai.api_key = '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'
 
 
         # Call ChatGPT API to generate a response
@@ -560,6 +933,37 @@ def generate_chatgpt_response(message):
     )
 
     return response.choices[0].message.content.strip()
+
+
+def get_step_count():
+    if 'image' not in request.files:
+        return '0'
+
+    image_file = request.files['image']
+    image = Image.open(io.BytesIO(image_file.read()))
+
+    ocr_text = pytesseract.image_to_string(image)
+    step_count = extract_numerical_part(ocr_text)
+
+    
+    username = session.get('username')
+    if username:
+        tracker_filename = f'tracker_{username}.txt'
+        with open(tracker_filename, 'a') as tracker_file:
+            tracker_file.write(f"Steps:{step_count}\n")
+
+   
+        users = get_users()
+        if username in users:
+            users[username]['points'] = str(int(users[username]['points']) + step_count // 1000 * 10)
+            update_users(users)
+
+    return redirect(url_for('index'))
+
+def extract_numerical_part(text):
+    numerical_part = ''.join(filter(str.isdigit, text))
+    return int(numerical_part) if numerical_part else 0
+
     
 if __name__ == '__main__':
     socketio.run(app)
--- FitnessApp/static/js/script.js
+++ FitnessApp/static/js/script.js
@@ -1,3 +1,27 @@
+function resizeImage(imageElement, maxWidth, maxHeight) {
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
+    let width = imageElement.width;
+    let height = imageElement.height;
+
+    if (width > height) {
+        if (width > maxWidth) {
+            height *= maxWidth / width;
+            width = maxWidth;
+        }
+    } else {
+        if (height > maxHeight) {
+            width *= maxHeight / height;
+            height = maxHeight;
+        }
+    }
+
+    canvas.width = width;
+    canvas.height = height;
+    ctx.drawImage(imageElement, 0, 0, width, height);
+    return canvas.toDataURL('image/jpeg', 0.7); 
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const postImages = document.querySelectorAll('.post img');
     postImages.forEach(function(img) {
